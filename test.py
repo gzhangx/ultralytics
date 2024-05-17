3f9d740e100c5234923e17581b88fb3cd00fc120
Gang Zhang <gzhangx@hotmail.com>
2024-05-16 21:56:44 -0400

    onnx

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ model = YOLO('cocbest.pt')
 #results = model.train(data='/work/cur/ccauto2/yolodata/coc/yolo8.yaml', epochs=10, imgsz=640, workers=0)
 
 # Perform object detection on an image using the model
-results = model('../ccauto2/yolodata/coc/images/all/testfullimg_cap_2024-01-13-044136.png', imgsz=640, workers=0)
+results = model('../../../../work/cur/ccauto2/yolodata/coc/images/all/testfullimg_cap_2024-01-13-044136.png', imgsz=640, workers=0)
 
 print(results[0].boxes.xywh,'results.0','results[0].boxes.cls',results[0].boxes.cls)
+
+path = model.export(format="onnx")
+print('path',path)
