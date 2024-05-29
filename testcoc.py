814a0a9a211bdf003b3438eed3916d06901592dc
Gang Zhang <gzhangx@hotmail.com>
2024-05-29 11:18:19 -0400

    train and test

---
+++ testcoc.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+import shutil
+import os
+
+model = YOLO('cocbest992.pt')
+
+imgFileName = '/utils/src/vision/yolodata/tt.png'
+results = model(imgFileName, imgsz=992, workers=0)
+
+print(results[0].boxes.xywh,'results.0','results[0].boxes.cls',results[0].boxes.cls)
+
+img = cv2.imread(imgFileName)
+for rec in results[0].boxes.xywh:    
+    xy = (int(rec[0].item()), int(rec[1].item()))
+    wh = (int(rec[2].item()), int(rec[3].item())) 
+    xy = (xy[0] - int(wh[0]/2), xy[1] - int(wh[1]/2))
+    wh = (wh[0] + xy[0], wh[1] + xy[1])
+    print(xy, 'xy', wh)
+    r = cv2.rectangle(img, xy, wh, color=(255,255,0), thickness=2)
+
+cv2.imwrite('testout.jpg', img)
+
+
